1d07e47011df65848a16530e93431e27f8c243d3
manthanjain6106 <jainmanthan6106@gmail.com>
2025-02-18 14:19:57 +0530

    hero section text size changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,11 +7,11 @@ export function Hero() {
   return (
     <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 justify-center items-center mt-10">
+        <h1 className="text-4xl md:text-8xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Spotlight <br /> which is not overused.
         </h1>
-        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto text-lg md:text-xl">
           A subtle yet effective spotlight effect, because the previous version
           is used a bit too much these days.
         </p>
